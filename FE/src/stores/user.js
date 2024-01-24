3e9f5f9dbed37209b2ad86c485836962f80de703
김준섭 <wns1915@naver.com>
2024-01-24 13:17:21 +0900

    [feat] : add user Store (api)

--- FE/src/stores/user.js
+++ FE/src/stores/user.js
@@ -10,6 +10,19 @@ export const useuserStore = defineStore("user", () => {
   const userId = ref("");
   const userName = ref("");
 
+  const userRDI = ref({}); // RDI - Recommended Daily Intake (당일 권장량)
+  const userRDICaffeine = ref(0.0);
+  const userRDISugar = ref(0.0);
+
+  const userMax = ref({});
+  const userMaxCaffeine = ref(0.0);
+  const userMaxCaffeineDate = ref("");
+  const userMaxSugar = ref(0.0);
+  const userMaxSugarDate = ref("");
+
+  const recommendedCaffeine = ref({});
+  const recommendedSugar = ref({});
+
   // =========== GETTER ===============
 
   const getUser = computed(() => {
@@ -24,7 +37,61 @@ export const useuserStore = defineStore("user", () => {
     return userName.value;
   });
 
+  const getUserRDI = computed(() => {
+    return userRDI.value;
+  });
+
+  const getUserRDICaffeine = computed(() => {
+    return userRDICaffeine.value;
+  });
+
+  const getUserRDISugar = computed(() => {
+    return userRDISugar.value;
+  });
+
+  const getUserMax = computed(() => {
+    return userMax.value;
+  });
+
+  const getUserMaxCaffeine = computed(() => {
+    return userMaxCaffeine.value;
+  });
+
+  const getUserMaxCaffeineDate = computed(() => {
+    return userMaxCaffeineDate.value;
+  });
+
+  const getUserMaxSugar = computed(() => {
+    return userMaxSugar.value;
+  });
+
+  const getUserMaxSugarDate = computed(() => {
+    return userMaxSugarDate.value;
+  });
+
+  const getRecommendedCaffeine = computed(() => {
+    return recommendedCaffeine.value;
+  });
+
+  const getRecommendedSugar = computed(() => {
+    return recommendedSugar.value;
+  });
+
   // =========== ACTION ===============
+
+  const sendKakaoToken = function (token) {
+    axios({
+      url: `${import.meta.env.REST_USER_API}/social-login`,
+      method: "POST",
+      data: user,
+    })
+      .then(() => {
+        // 카카오 에서 받아온 토큰을 백으로 전달
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const createUser = function (user) {
     axios({
@@ -41,10 +108,42 @@ export const useuserStore = defineStore("user", () => {
   const researchUser = function () {
     axios({
       url: import.meta.env.REST_USER_API,
-      method: "get",
+      method: "GET",
     })
       .then((res) => {
         user.value = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const researchAmount = function () {
+    axios({
+      url: `${import.meta.env.REST_USER_API}/amount`,
+      method: "GET",
+    })
+      .then((res) => {
+        userRDI.value = res.data;
+        userRDICaffeine.value = userRDI.value.userCaffeine;
+        userRDISugar.value = userRDI.value.userSugar;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const researchMax = function () {
+    axios({
+      url: `${import.meta.env.REST_USER_API}/max`,
+      method: "GET",
+    })
+      .then((res) => {
+        userMax.value = res.data;
+        userMaxCaffeine.value = userMax.value.maxCaffeineValue;
+        userMaxCaffeineDate.value = userMax.value.maxCaffeineDate;
+        userMaxSugar.value = userMax.value.maxSugarValue;
+        userMaxSugarDate.value = userMax.value.maxSugarDate;
       })
       .catch((err) => {
         console.log(err);
@@ -74,6 +173,32 @@ export const useuserStore = defineStore("user", () => {
       });
   };
 
+  const researchRecommendSugar = function () {
+    axios({
+      url: `${import.meta.env.REST_USER_API}/recommendsugar`,
+      method: "GET",
+    })
+      .then((res) => {
+        recommendedSugar.value = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const researchRecommendCaffeine = function () {
+    axios({
+      url: `${import.meta.env.REST_USER_API}/recommendcaffeine`,
+      method: "GET",
+    })
+      .then((res) => {
+        recommendedCaffeine.value = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const isAdditionalInfoFilled = computed(() => {
     return user.value.additionalInfo && user.value.additionalInfo.trim() !== "";
   }); // 추가정보가 입력되어있는지 아닌지 확인하기 위한 함수
@@ -82,13 +207,38 @@ export const useuserStore = defineStore("user", () => {
     user,
     userId,
     userName,
+    userRDI,
+    userRDICaffeine,
+    userRDISugar,
+    userMax,
+    userMaxCaffeine,
+    userMaxCaffeineDate,
+    userMaxSugar,
+    userMaxSugarDate,
+    recommendedCaffeine,
+    recommendedSugar,
     getUser,
     getUserId,
     getUserName,
+    getUserRDI,
+    getUserRDICaffeine,
+    getUserRDISugar,
+    getUserMax,
+    getUserMaxCaffeine,
+    getUserMaxCaffeineDate,
+    getUserMaxSugar,
+    getUserMaxSugarDate,
+    getRecommendedCaffeine,
+    getRecommendedSugar,
+    sendKakaoToken,
     createUser,
     researchUser,
+    researchAmount,
+    researchMax,
     updateUser,
     deleteUser,
+    researchRecommendSugar,
+    researchRecommendCaffeine,
     isAdditionalInfoFilled,
   };
 });
