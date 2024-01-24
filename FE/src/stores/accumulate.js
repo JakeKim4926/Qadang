2e563d088a54d4c53532004852a069d014824106
김준섭 <wns1915@naver.com>
2024-01-24 11:10:08 +0900

    [feat] add drinks store (api)

--- FE/src/stores/accumulate.js
+++ FE/src/stores/accumulate.js
@@ -3,107 +3,104 @@ import { defineStore } from "pinia";
 import router from "@/router";
 import axios from "axios";
 
-export const useAccumulateStore = defineStore(
-  "accumulate",
-  () => {
-    // =========== STATE ===============
+export const useAccumulateStore = defineStore("accumulate", () => {
+  // =========== STATE ===============
 
-    const acuumulateList = ref([]);
-    const acuumulateMonth = ref([]);
+  const acuumulateList = ref([]);
+  const acuumulateMonth = ref([]);
 
-    const acuumulateToday = ref({});
-    const acuumulateDay = ref({});
+  const acuumulateToday = ref({});
+  const acuumulateDay = ref({});
 
-    // =========== GETTER ===============
+  // =========== GETTER ===============
 
-    const getAcuumulateList = computed(() => {
-      return acuumulateList.value;
-    });
+  const getAcuumulateList = computed(() => {
+    return acuumulateList.value;
+  });
 
-    const getAcuumulateMonth = computed(() => {
-      return acuumulateMonth.value;
-    });
+  const getAcuumulateMonth = computed(() => {
+    return acuumulateMonth.value;
+  });
 
-    const getAcuumulateToday = computed(() => {
-      return acuumulateToday.value;
-    });
+  const getAcuumulateToday = computed(() => {
+    return acuumulateToday.value;
+  });
 
-    const getAcuumulateDay = computed(() => {
-      return acuumulateDay.value;
-    });
+  const getAcuumulateDay = computed(() => {
+    return acuumulateDay.value;
+  });
 
-    // =========== ACTION ===============
-    // params는 요청과 함께 전송되는 파라미터 (쿼리스트링)
-    // data는 요청 바디로 전송될 데이터 (JSON)
-    const today = function () {
-      axios({
-        url: `${import.meta.env.REST_USER_API}/today`,
-        method: "GET",
+  // =========== ACTION ===============
+  // params는 요청과 함께 전송되는 파라미터 (쿼리스트링)
+  // data는 요청 바디로 전송될 데이터 (JSON)
+  const today = function () {
+    axios({
+      url: `${import.meta.env.REST_ACCUMULATE_API}/today`,
+      method: "GET",
+    })
+      .then((res) => {
+        acuumulateToday.value = res.data;
       })
-        .then((res) => {
-          acuumulateToday.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const duration = function () {
-      axios({
-        url: `${import.meta.env.REST_USER_API}/duration`,
-        method: "GET",
+  const duration = function () {
+    axios({
+      url: `${import.meta.env.REST_ACCUMULATE_API}/duration`,
+      method: "GET",
+    })
+      .then((res) => {
+        acuumulateList.value = res.data;
       })
-        .then((res) => {
-          acuumulateList.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    // date example ) 202401 (연도+월)
-    const month = function (date) {
-      axios({
-        url: `${import.meta.env.REST_USER_API}/month`,
-        method: "GET",
-        params: { ym: date },
+  // date example) 202401 (연도+월)
+  const month = function (date) {
+    axios({
+      url: `${import.meta.env.REST_ACCUMULATE_API}/month`,
+      method: "GET",
+      params: { ym: date },
+    })
+      .then((res) => {
+        acuumulateMonth.value = res.data;
       })
-        .then((res) => {
-          acuumulateMonth.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    // date example ) 20240123 (연도+월일)
-    const day = function (date) {
-      axios({
-        url: `${import.meta.env.REST_USER_API}/day`,
-        method: "GET",
-        params: { date: date },
+  // date example) 20240123 (연도+월일)
+  const day = function (date) {
+    axios({
+      url: `${import.meta.env.REST_ACCUMULATE_API}/day`,
+      method: "GET",
+      params: { date: date },
+    })
+      .then((res) => {
+        acuumulateDay.value = res.data;
       })
-        .then((res) => {
-          acuumulateDay.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return {
-      acuumulateList,
-      acuumulateMonth,
-      acuumulateToday,      
-      acuumulateDay,
-      getAcuumulateList,
-      getAcuumulateMonth,
-      getAcuumulateToday,
-      getAcuumulateDay,
-      today,
-      duration,
-      month,
-      day,
-    };
-  },
-);
+  return {
+    acuumulateList,
+    acuumulateMonth,
+    acuumulateToday,
+    acuumulateDay,
+    getAcuumulateList,
+    getAcuumulateMonth,
+    getAcuumulateToday,
+    getAcuumulateDay,
+    today,
+    duration,
+    month,
+    day,
+  };
+});
--- FE/src/stores/user.js
+++ FE/src/stores/user.js
@@ -3,98 +3,92 @@ import { defineStore } from "pinia";
 import router from "@/router";
 import axios from "axios";
 
-export const useuserStore = defineStore(
-  "user",
-  () => {
-    // =========== STATE ===============
+export const useuserStore = defineStore("user", () => {
+  // =========== STATE ===============
 
-    const user = ref({});
-    const userId = ref('');
-    const userName = ref('');
+  const user = ref({});
+  const userId = ref("");
+  const userName = ref("");
 
-    // =========== GETTER ===============
+  // =========== GETTER ===============
 
-    const getUser = computed(() => {
-      return user.value;
-    });
+  const getUser = computed(() => {
+    return user.value;
+  });
 
-    const getUserId = computed(() => {
-      return userId.value;
-    });
+  const getUserId = computed(() => {
+    return userId.value;
+  });
 
-    const getUserName = computed(() => {
-      return userName.value;
-    });
+  const getUserName = computed(() => {
+    return userName.value;
+  });
 
-    // =========== ACTION ===============
+  // =========== ACTION ===============
 
-    const createUser = function (user) {
-      axios({
-        url: import.meta.env.REST_USER_API,
-        method: "POST",
-        data: user,
+  const createUser = function (user) {
+    axios({
+      url: import.meta.env.REST_USER_API,
+      method: "POST",
+      data: user,
+    })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const researchUser = function () {
+    axios({
+      url: import.meta.env.REST_USER_API,
+      method: "get",
+    })
+      .then((res) => {
+        user.value = res.data;
       })
-        .then(() => {
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const researchUser = function () {
-        axios({
-          url: import.meta.env.REST_USER_API,
-          method: "get",
-        })
-          .then((res) => {
-            user.value = res.data
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+  const updateUser = function () {
+    axios({
+      url: import.meta.env.REST_USER_API,
+      method: "PUT",
+      data: user.value,
+    })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const updateUser = function () {
-      axios({
-        url: import.meta.env.REST_USER_API,
-        method: "PUT",
-        data: user.value,
-      })
-        .then(() => {
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+  const deleteUser = function () {
+    axios({
+      url: import.meta.env.REST_USER_API,
+      method: "DELETE",
+    })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const deleteUser = function () {
-        axios({
-          url: import.meta.env.REST_USER_API,
-          method: "DELETE",
-        })
-          .then(() => {
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
-    
-    const isAdditionalInfoFilled = computed(() => {
-      return user.value.additionalInfo && user.value.additionalInfo.trim() !== '';
-    }); // 추가정보가 입력되어있는지 아닌지 확인하기 위한 함수
+  const isAdditionalInfoFilled = computed(() => {
+    return user.value.additionalInfo && user.value.additionalInfo.trim() !== "";
+  }); // 추가정보가 입력되어있는지 아닌지 확인하기 위한 함수
 
-    return {
-      user,
-      userId,
-      userName,
-      getUser,
-      getUserId,
-      getUserName,
-      createUser,
-      researchUser,
-      updateUser,
-      deleteUser,
-      isAdditionalInfoFilled,
-    }
-  },  
-);
+  return {
+    user,
+    userId,
+    userName,
+    getUser,
+    getUserId,
+    getUserName,
+    createUser,
+    researchUser,
+    updateUser,
+    deleteUser,
+    isAdditionalInfoFilled,
+  };
+});
