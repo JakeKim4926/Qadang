145c8870995d878719e72e3ecbfe4fca1a8f29d3
Jake_Kim <wer1915@gmail.com>
2024-01-29 21:36:05 +0900

    [plus] add socket io client

--- FE/src/views/HomeView.vue
+++ FE/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>hello home</h2>
+    <RouterLink to="/chat"><font-awesome-icon :icon="['fas', 'comments']" class="footer-icon" /></RouterLink>
   </div>
 </template>
   
