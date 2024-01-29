af1566777bcb683dca4e786e8c0ad2c4feba8985
김준섭 <wns1915@naver.com>
2024-01-29 23:11:27 +0900

    [feat] add socket store and test

--- FE/src/views/chat/ChatView.vue
+++ FE/src/views/chat/ChatView.vue
@@ -1,12 +1,142 @@
 <template>
-    <div>
-      <h2>Hello Chat</h2>
+  <div>
+    <h2>Hello Chat</h2>
+    <div class="chat-container">
+      <div class="chat-messages" ref="chatContainer">
+        <div v-for="(msg, index) in socketStore.getChatMessages" :key="index"
+          :class="msg.owner == id ? 'my-chat' : 'their-chat'">
+          <div class="message">{{ msg.message }}</div>
+        </div>
+      </div>
+      <textarea style="resize: none" v-model="message" class="chat-input" placeholder="메시지 입력"
+        @keydown.enter="sendMessage" />
     </div>
-  </template>
+
+  </div>
+</template>
   
-  <script setup>
+<script setup>
+import { nextTick, ref, watchEffect, onMounted } from "vue";
+import { useSocketStore, messageType } from '@/stores/socket';
+
+const socketStore = useSocketStore();
+
+const message = ref('')
+const chatContainer = ref(null);
+
+onMounted(() => {
   
-  </script>
+  const randomInteger = Math.ceil(Math.random() * 10);
+  socketStore.id = randomInteger;
+
+})
+
+socketStore.socket.on("connect", () => {
+  socketStore.connected = ref(true);
+});
+
+socketStore.socket.on("disconnect", () => {
+  socketStore.connected = ref(false);
+});
+
+socketStore.socket.on('chat', (data) => {
+  console.log(data.message)
+  socketStore.chatMessages.push(data)
+})
+
+function sendMessage() {
+  const chat = {
+    owner: socketStore.id,
+    message: message.value
+  }
+  console.log('??22 : ', socketStore.id)
+  console.log('?? : ', message.value)
+  socketStore.chatMessages.push(chat)
+  socketStore.socket.timeout(5000).emit('chat', chat)
+
+  message.value = "";
+  // 스크롤을 새 메시지 아래로 이동시킵니다.
+  nextTick(() => {
+    scrollChatToBottom();
+  });
+}
+
+function adjustTextarea() {
+}
+
+function scrollChatToBottom() {
+  if (chatContainer.value) {
+    chatContainer.value.scrollTop = chatContainer.value.scrollHeight;
+  }
+}
+
+watchEffect(() => {
+  scrollChatToBottom();
+  console.log(socketStore.chatMessages.value)
+})
+
+</script>
   
-  <style scoped>
-  </style>
+<style scoped>
+.chat-container {
+  border: 1px solid cornflowerblue;
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  padding: 20px;
+}
+
+.chat-messages {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+}
+
+.my-chat {
+  align-self: flex-end;
+  background-color: #0084ff;
+  color: #fff;
+  max-width: 70%;
+  padding: 8px;
+  margin: 8px 8px 8px 0;
+  border-radius: 6px;
+  text-align: right;
+  position: relative;
+}
+
+.their-chat {
+  align-self: flex-start;
+  background-color: #f0f0f0;
+  max-width: 70%;
+  padding: 8px;
+  margin: 8px 0;
+  border-radius: 6px;
+  text-align: left;
+  color: #282828;
+  position: relative;
+}
+
+.chat-input {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+input[type="text"] {
+  flex: 1;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+button {
+  background-color: #0084ff;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
