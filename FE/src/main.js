c7f19653093ac55c1679299a88f4d27b54d43d2d
임지현 <acubens03@gmail.com>
2024-01-29 10:29:06 +0900

    [plus] : add dependencies

--- FE/src/main.js
+++ FE/src/main.js
@@ -5,8 +5,26 @@ import { useRecordsStore } from './stores/records'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
