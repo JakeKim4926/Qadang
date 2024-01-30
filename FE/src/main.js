c24acbad76e659cf12bbaa32a1f11ceca33aae3b
김준섭 <wns1915@naver.com>
2024-01-30 13:37:50 +0900

    [feat] add chat api

--- FE/src/main.js
+++ FE/src/main.js
@@ -24,16 +24,16 @@ library.add(fas, far, fab)
 
 const app = createApp(App)
 
-// Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+// // Vuetify
+// import "vuetify/styles";
+// import { createVuetify } from "vuetify";
+// import * as components from "vuetify/components";
+// import * as directives from "vuetify/directives";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+// });
 
 
 app.component('font-awesome-icon', FontAwesomeIcon)
