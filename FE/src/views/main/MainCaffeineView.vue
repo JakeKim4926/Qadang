34b59a202f133e0c641918db8973204c5bce89fe
임지현 <acubens03@gmail.com>
2024-02-01 14:55:57 +0900

    [feat] add main skeleton code

--- FE/src/views/main/MainCaffeineView.vue
+++ FE/src/views/main/MainCaffeineView.vue
@@ -2,6 +2,39 @@
   <div>
     <h2>Hello Main Caffeine</h2>
   </div>
+
+  <p>닉네임 님</p>
+  <div>
+    당 섭취량으로 가는 버튼
+  </div>
+
+  <p>카페인 섭취량</p>
+  <div>
+    <p>이미지</p>
+    <div>
+      <p>방금 마신 음료</p>
+      <p>카페인 mg</p>
+      <p>하루 총합 섭취량 / 권장량</p>
+      <p>000.mg/400mg</p>
+    </div>
+    <div>
+      <p>혈중 카페인농도 만땅</p>
+    </div>
+  </div>
+
+  <div>
+    최근에 마신 카페인을 한눈에 보아요
+    chart.js 활용
+  </div>
+
+  <div>
+    오늘은 이 음료 어때요?
+    추천 음료 상세보기
+  </div>
+
+  <div>
+    채팅으로 가기
+  </div>
 </template>
 
 <script setup>
--- FE/src/views/main/MainSugarView.vue
+++ FE/src/views/main/MainSugarView.vue
@@ -2,6 +2,39 @@
   <div>
     <h2>Hello Main Sugar</h2>
   </div>
+
+  <p>닉네임 님</p>
+  <div>
+    카페인 섭취량으로 가는 버튼
+  </div>
+
+  <p>당 섭취량</p>
+  <div>
+    <p>이미지</p>
+    <div>
+      <p>방금 마신 음료</p>
+      <p>당 g</p>
+      <p>하루 총합 섭취량 / 권장량</p>
+      <p>000.g/400g</p>
+    </div>
+    <div>
+      <p>피가 끈적거려요</p>
+    </div>
+  </div>
+
+  <div>
+    최근에 마신 당을 한눈에 보아요
+    chart.js 활용
+  </div>
+
+  <div>
+    오늘은 이 음료 어때요?
+    추천 음료 상세보기
+  </div>
+
+  <div>
+    채팅으로 가기
+  </div>
 </template>
 
 <script setup>
