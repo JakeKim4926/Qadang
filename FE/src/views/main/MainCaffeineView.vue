8c13925dad219a2b86b82127a3c3ac4038b5a77c
임지현 <acubens03@gmail.com>
2024-02-01 20:42:46 +0900

    [feat] add load data function

--- FE/src/views/main/MainCaffeineView.vue
+++ FE/src/views/main/MainCaffeineView.vue
@@ -14,7 +14,7 @@
     <p>이미지</p>
     <div>
       <p>방금 마신 음료</p>
-      <p>카페인 mg</p>
+      <p>카페인 {{ getRecentCaffeine }}mg</p>
       <p>하루 총합 섭취량 / 권장량</p>
       <p>{{ accumulateStore.getAcuumulateToday.accumulateCaffeine }} / 
         {{ accumulateStore.getAcuumulateToday.userCaffeine }}mg</p>
@@ -47,15 +47,44 @@ import router from '@/router';
 
 import { useUserStore } from '@/stores/user';
 import { useAccumulateStore } from '@/stores/accumulate';
+import { useRecordsStore } from '@/stores/records';
+import { DayCellContainer } from '@fullcalendar/core/internal';
 
 const userStore = useUserStore()
 const accumulateStore = useAccumulateStore()
+const recordsStore = useRecordsStore()
+
+const recentCaffeine = ref(0)
+
+const getRecentCaffeine = computed(() => {
+  return recentCaffeine.value
+})
 
 // 데이터를 가져오기 위한 함수
-onMounted(() => {
-  userStore.researchUser()
-  accumulateStore.duration()
-  userStore.researchRecommendCaffeine()
+onMounted(async () => {
+
+  // 현재 날짜를 알기 위한 변수
+  const todayDate = new Date()
+  const year = todayDate.getFullYear()
+  let month = todayDate.getMonth()
+  let day = todayDate.getDate()
+
+  month = month < 10 ? '0' + month.toString() : month.toString()
+  day = day < 10 ? '0' + day.toString() : day.toString()
+
+  const date = ref(null)
+  date.value = year + month + day
+
+  await userStore.researchUser()                   // 닉네임
+  await accumulateStore.today()                   // 권장량, 섭취량
+  await accumulateStore.duration()                // chart.js를 위한 기간별 섭취량
+  await userStore.researchRecommendCaffeine()     // 기록 기반 음료추천 카페인
+  await recordsStore.researchDayDrink(date)       // 방금 마신 음료 계산을 위한 일자별 기록
+
+  // 최근 마신 음료의 카페인 계산
+  const idx = recordsStore.getDayDrink.length - 1
+  const recentDrink = recordsStore.getDayDrink[idx]
+  recentCaffeine.value = recentDrink ? recentDrink.drinkCaffeine.value : 0
 })
 
 // 채팅으로 이동
