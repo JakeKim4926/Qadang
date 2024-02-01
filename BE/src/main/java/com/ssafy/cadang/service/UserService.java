1dac68164bc54ec399f6e31292de54bac5d4cc20
김보경 <love2491193@naver.com>
2024-02-01 13:08:45 +0900

    [fix] user service findbyName

--- BE/src/main/java/com/ssafy/cadang/service/UserService.java
+++ BE/src/main/java/com/ssafy/cadang/service/UserService.java
@@ -1,6 +1,28 @@
 package com.ssafy.cadang.service;
 
+import com.ssafy.cadang.domain.User;
+import com.ssafy.cadang.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public User findUser(Long userId) {
+        return userRepository.findByUserId(userId);
+    }
+
+    public void update(User user) {
+        userRepository.save(user);
+    }
+
+    public String findUserName(Long userId) {
+        User user = findUser(userId);
+        return user.getUserName();
+    }
+
 
     // 회원 정보 입력
 
