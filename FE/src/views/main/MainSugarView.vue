88f2e29169be83f58bd6abba579463b2d47a003d
임지현 <acubens03@gmail.com>
2024-02-02 15:15:05 +0900

    [plus] add dependency

--- FE/src/views/main/MainSugarView.vue
+++ FE/src/views/main/MainSugarView.vue
@@ -14,15 +14,11 @@ export default {
     data: {
       labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
       datasets: [{
-        label: '# of Votes',
+        label: '기간별 카페인 섭취량',
         data: [ 12, 19, 3, 5, 2, 3 ],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
+          '#846046',
         ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-        ],
-        borderWidth: 1
       }]
     },
     options: {
