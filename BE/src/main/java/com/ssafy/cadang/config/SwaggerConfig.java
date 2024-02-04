9e00e5709b13b27665867d1202057dbd6ce68365
bora <bbk4281@naver,com>
2024-02-04 14:51:30 +0900

    [chore] plus IdResponse

--- BE/src/main/java/com/ssafy/cadang/config/SwaggerConfig.java
+++ BE/src/main/java/com/ssafy/cadang/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.cadang.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
