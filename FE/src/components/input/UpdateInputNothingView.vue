e79485c81f96b533b80cff484085a8b18ea2fd35
임지현 <acubens03@gmail.com>
2024-02-05 16:45:14 +0900

    [feat] add update input skeleton code

---
+++ FE/src/components/input/UpdateInputNothingView.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="board-create-container">
+    <div class="update-box">
+      <div class="close" @click="closeUpdateNothingModal">
+        <font-awesome-icon :icon="['fas', 'circle-xmark']" style="color: #000000;" size="xl"/>
+      </div>
+
+      <div class="update-container">
+        <div>
+          <h2>카페 음료를 입력하여 수정해주세요</h2>
+        </div>
+
+        <div>
+          <label for="cafeName" class="big-font">카페명</label>
+          <input type="text" name="cafeName" id="cafeName"
+          v-model="cafeName" class="button_select select"/>
+        </div>
+
+        <div>
+          <label for="drinkName" class="big-font">음료명</label>
+          <input type="text" name="drinkName" id="drinkName"
+          v-model="drinkName" class="button_select select"/>
+        </div>
+
+        <div>
+          <label for="drinkCaffeine" class="big-font">카페인</label>
+          <input type="number" name="drinkCaffeine" id="drinkCaffeine"
+          v-model="drinkCaffeine" class="button_select select">
+        </div>
+
+        <div>
+          <label for="drinkSugar" class="big-font">　　당</label>
+          <input type="number" name="drinkSugar" id="drinkSugar"
+          v-model="drinkSugar" class="button_select select">
+        </div>
+
+        <div class="item-container">
+          <button @click="drinkUpdateSubmit" class="button_caffeine buttons">입력완료</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRecordsStore } from "@/stores/records";
+import { isUpdateNothingModal } from '@/stores/util';
+
+const store = useRecordsStore()
+
+const cafeName = ref(null)
+const drinkName = ref(null)
+const drinkCaffeine = ref(0)
+const drinkSugar = ref(0)
+
+// 모달창을 닫기 위한 함수
+const closeUpdateNothingModal = () => {
+  isUpdateNothingModal.value = false
+}
+
+// 음료 업데이트를 위한 함수
+const drinkUpdateSubmit = () => {
+  if (isValid()) {
+    console.log('입력값이 올바릅니다. 데이터를 전송합니다.')
+
+    const drink = {
+      recordId: null,             // 음료 업데이트를 하면서 가져오는 recordid값
+      cafeName: cafeName.value,
+      drinkName: drinkName.value,
+      drinkCaffeine: drinkCaffeine.value.toFixed(2),
+      drinkSugar: drinkSugar.value.toFixed(2)
+    }
+
+    console.log(drink)
+
+    // 유효한 데이터를 백엔드로 전송 및 창 닫기
+    store.updateMyDrink(drink)
+    alert('입력값이 올바릅니다. 데이터를 전송합니다.')
+    closeUpdateNothingModal()
+
+    } else {
+      console.log('입력값이 올바르지 않습니다')
+      alert('입력값이 올바르지 않습니다. 다시 확인해주세요.')
+    }
+  }
+</script>
+
+<style scoped>
+@import "../../components/font/font.css";
+@import "../../components/color/color.css";
+
+h2 {
+  color: #562B1A;
+  font-weight: bold;
+  text-align: center;
+}
+
+div {
+  margin: 10px;
+}
+
+.board-create-container {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  /* max-width:614px;
+  max-height: 584px; */
+  background-color: rgba(128, 128, 128, 0.863) !important;
+  top: 0;
+  left: 0;
+  margin: 0;
+  z-index: 99 !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.update-box {
+  background: #ffffff;
+  border-radius: 30px;
+  border-style: solid;
+  border-color: #d9d9d9;
+  border-width: 1px;
+  position: absolute;
+  width: 500px;
+  height: 450px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+
+.update-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.item-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.close {
+  display: flex;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+
+.big-font {
+  margin-right: 10px;
+  font-size: 20px;
+}
+
+.select {
+  border-radius: 22px;
+  border: 2px solid #846046;
+  padding: 10px 20px;
+  width: 353px;
+}
+
+.button_input_color {
+  margin-right: 5px;
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.button_caffeine {
+  margin-left: 80px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+}
+.buttons {
+  width: 150px;
+  height: 60px;
+  border: none;
+}
+
+.tip-container {
+  position: relative;
+}
+
+.tip {
+  position: fixed;
+  top: 63%;
+  left: 63%;
+  transform: translate(-50%, -50%);
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  z-index: 100;
+}
+</style>
